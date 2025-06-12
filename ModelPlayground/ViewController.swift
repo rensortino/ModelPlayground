1d949f8811b1557e559465b93e6ae15ce3750a2e
Renato Sortino <rensortino@gmail.com>
2025-06-12 17:03:42 +0200

    Initial Commit

---
+++ ModelPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModelPlayground
+//
+//  Created by Renato Sortino on 12/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ModelPlaygroundTests/ModelPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  ModelPlaygroundTests.swift
+//  ModelPlaygroundTests
+//
+//  Created by Renato Sortino on 12/06/2025.
+//
+
+import Testing
+@testable import ModelPlayground
+
+struct ModelPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
